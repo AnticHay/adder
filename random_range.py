f82be35a8cb7a0726fb2b7ab5518de106fa1c07c
ichocky <61056590+ichocky@users.noreply.github.com>
2020-06-17 17:10:03 +0100

    Add files via upload

---
+++ random_range.py
@@ -0,0 +1,51 @@
+import random
+for i in range(10):
+  a = random.randrange(1,100)
+  b = random.randrange(1,100)
+  c = random.randrange(1,100)
+  d = random.randrange(1,100)
+  e = random.randrange(1,100)
+  f = random.randrange(1,100)
+  g = random.randrange(1,100)
+  h = random.randrange(1,100)
+  i = random.randrange(1,100)
+  j = random.randrange(1,100)
+
+  list1 =[a,b,c,d,e,f,g,h,i,j] 
+  list1.sort()
+
+for num in list1:
+    print (num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+   
+ 
+ 
+
+
+   
+
+    
